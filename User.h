4202df725fca7bf7065ba036ee5707683e1f801f
VINAGHOST <nguyentrungnhan87@gmail.com>
2019-04-23 08:40:15 +0700

    Add more info to User

--- User.h
+++ User.h
@@ -6,8 +6,16 @@
 struct User {
 	char name[50];
 	char password[50];
-	int permission;
+	unsigned int permission;
+
+	char HoTen[50];
+	char MS[10];
+	char Birth[9];
+	char DiaChi[20];
+	int Nam;
+	int active;
 };
+
 
 void initUser(User &t);
 
