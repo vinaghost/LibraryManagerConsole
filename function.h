c68145623f81885356942fac479c5f76e1cf6b3b
kudo1212000 <tieunhishockway121@gmail.com>
2019-05-02 00:57:36 +0700

    update for book

--- function.h
+++ function.h
@@ -2,6 +2,8 @@
 #include "const.h"
 #include "User.h"
 #include "LinkedListReader.h"
+#include "LinkedListBook.h"
+
 
 
 
@@ -9,3 +11,8 @@ FUNCTION_CASE viewReader(User u, ListReader lReader);
 FUNCTION_CASE addReader(User u, ListReader &lReader);
 FUNCTION_CASE findReaderAsName(User u, ListReader &lReader, ListReader &findReader);
 FUNCTION_CASE findReaderAsCMND(User u, ListReader &lReader, ListReader &findReader);
+
+FUNCTION_CASE viewBook(User u, ListBook lBook);
+FUNCTION_CASE addBook(User u, ListBook &lBook);
+FUNCTION_CASE findBookAsName(User u, ListBook &lBook, ListBook &findBook);
+FUNCTION_CASE findBookAsCMND(User u, ListBook &lBook, ListBook &findBook);
